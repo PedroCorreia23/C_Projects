6bb3eeedce132e55a72ac29bf43ae184588d7329
Pedro <75038468+PedroCorreia23@users.noreply.github.com>
2023-06-23 20:55:23 +0100

    Update calculator.c

--- Beginner/calculatorProject/calculator.c
+++ Beginner/calculatorProject/calculator.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX 1000
+
 void numberInsert(int *ins){
     printf("How many numbers do want to insert? ");
     scanf("%d", ins);
 }
-void askN(int *ins, float *arr){
+void askN(int *ins, float arr[MAX]){
 
 
     printf("Insert numbers: ");
@@ -15,7 +17,7 @@ void askN(int *ins, float *arr){
     }    
 }
 
-float add(int *ins, float *arr){
+float add(int *ins, float arr[MAX]){
 
     float sum = 0;
     for (int i = 0; i < *ins; i++)
@@ -26,7 +28,7 @@ float add(int *ins, float *arr){
     return sum;
 }
 
-float sub(int *ins, float *arr){
+float sub(int *ins, float arr[MAX]){
     float subt = arr[0];
     for (int i = 1; i < *ins; i++){
         subt -= arr[i];
@@ -34,7 +36,7 @@ float sub(int *ins, float *arr){
     return subt;
 }
 
-float mult(int *ins, float *arr){
+float mult(int *ins, float arr[MAX]){
 
     float mul = arr[0];  // initialize with the first element of array
     for (int i = 1; i < *ins; i++)  
@@ -44,7 +46,7 @@ float mult(int *ins, float *arr){
     return mul;
 }
 
-float division(int *ins, float *arr){
+float division(int *ins, float arr[MAX]){
     float divi = arr[0];
     for (int i = 1; i < *ins; i++){
         if(arr[i] != 0.0) // Add a condition to prevent division by zero
@@ -61,7 +63,7 @@ int main(){
 
     int opcao = -1, numIn;//opcao and  amount of numbers the user want to insert;
     int *ins = &numIn; 
-    float *arr;
+    float arr[MAX];
     float sum = 0;
 
     printf("----Calculator----\n");
@@ -76,41 +78,33 @@ int main(){
         {
         case 1:
             numberInsert(ins);
-            arr = (float*) malloc(*ins * sizeof(float));  // Allocate memory for array
             askN(ins, arr);
             sum = add(ins, arr);
-            printf("Total sum: %.2f\n", sum);
-            free(arr);            
+            printf("Total sum: %.2f\n", sum);            
             break;
         case 2:
-            numberInsert(ins);
-            arr = (float*) malloc(*ins * sizeof(float));  
+            numberInsert(ins);  
             askN(ins, arr);
             float subt = sub(ins, arr);
-            printf("Total Sub: %.2f\n", subt);
-            free(arr);            
+            printf("Total Sub: %.2f\n", subt);            
             break;
         case 3:
             numberInsert(ins);
-            arr = (float*) malloc(*ins * sizeof(float));  
             askN(ins, arr);
             float mul = mult(ins, arr);
-            printf("Total Multiplication: %.2f\n", mul);
-            free(arr);            
+            printf("Total Multiplication: %.2f\n", mul);            
             break;
         case 4:
             numberInsert(ins);
-            arr = (float*) malloc(*ins * sizeof(float));  
             askN(ins, arr);
             float divi = division(ins, arr);
-            printf("Total Division: %.2f\n", divi);
-            free(arr);  
+            printf("Total Division: %.2f\n", divi);  
             break;          
         case 0:
             break;
         }
     }
-
+    free(arr);
 }
 
 

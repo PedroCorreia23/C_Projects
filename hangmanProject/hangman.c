09f82d25cc6741d84cbc81c2017a2dfa3892ad44
Pedro <75038468+PedroCorreia23@users.noreply.github.com>
2023-06-24 19:37:20 +0100

    Update hangman.c

--- hangmanProject/hangman.c
+++ hangmanProject/hangman.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define NUM_CATEGORIES 6
+
+void randWord(){
+
+    char *word;
+    srand(time(NULL));
+    word = animals[rand() % 3];
+    printf("%s", word);
+}
 
 void waitForEnter() {
     char ch = getchar();
@@ -10,11 +22,38 @@ void waitForEnter() {
 
 int main() {
 
-    printf("Press Enter to start the game...\n");
+    char *animals[] = {"dog", "cat", "penguin"};
+    char *countries[] = {"USA", "France", "Brazil"};
+    char *car_brands[] = {"Tesla", "BMW", "Toyota"};
+    char *colors[] = {"Red", "Green", "Blue"};
+    char *names[] = {"Alice", "Bob", "Charlie"};
+
+    char **all_categories[NUM_CATEGORIES] = {animals, countries, car_brands, colors, names, NULL};
+
+    char word;
+    int cat;
+    printf("Press Enter to start...\n");
     waitForEnter();
-    printf("Game started!\n");
 
+    printf("Categories:\n ");
+    printf("1. Animals\n2. Countries\n3. Car Brands\n4. Colors\n5. Names\n6. Mix Of All\n");
+    printf("Choose one: ");
+    scanf("%d", &cat);
+    getchar();  // Consume the '\n' character left by scanf
+    switch (cat)
+    {
+    case 1:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
 
+    printf("Game started!\n\n");
+
+    printf("Choosing word....");
+    randWord();
 
     return 0;
 }

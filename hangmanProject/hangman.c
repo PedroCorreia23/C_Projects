fe05258c269ee7d530a27688962ee580468bc0e2
Pedro <75038468+PedroCorreia23@users.noreply.github.com>
2023-06-24 20:52:56 +0100

    Update hangman.c

--- hangmanProject/hangman.c
+++ hangmanProject/hangman.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #define NUM_CATEGORIES 6
@@ -18,7 +19,7 @@ void waitForEnter() {
     }
 }
 
-int main() {
+void game() {
     char *animals[] = {"dog", "cat", "penguin"};
     char *countries[] = {"USA", "France", "Brazil"};
     char *car_brands[] = {"Tesla", "BMW", "Toyota"};
@@ -27,30 +28,76 @@ int main() {
 
     char **all_categories[NUM_CATEGORIES] = {animals, countries, car_brands, colors, names, NULL};
 
+    int cat, want, lives = 3;
+    char word[20];
+    char *chosenWord = NULL;
+
     printf("Press Enter to start...\n");
     waitForEnter();
 
-    printf("Categories:\n ");
+    printf("Categories:\n");
     printf("1. Animals\n2. Countries\n3. Car Brands\n4. Colors\n5. Names\n6. Random\n");
     printf("Choose one: ");
-    int cat;
     scanf("%d", &cat);
     getchar();  // Consume the '\n' character left by scanf
 
     if (cat >= 1 && cat <= NUM_CATEGORIES) {
-        printf("Game started!\n\n");
+        printf("\nGame started!\n\n");
         printf("Choosing word....\n");
-        char* chosenWord;
         if (cat == NUM_CATEGORIES) {
             // Random category
             chosenWord = randWord(all_categories[rand() % (NUM_CATEGORIES - 1)]);
         } else {
             chosenWord = randWord(all_categories[cat - 1]);
         }
-        printf("%s\n", chosenWord);  // Print the chosen word
+        printf("Word choosen: %s\n", chosenWord);  // Print the chosen word
     } else {
         printf("Invalid category number. Please run the program again.\n");
     }
 
+    while (lives > 0)
+    {
+        printf("What do you want to do?\n");
+        printf("1. Guess a letter\n2. Guess the word\n");
+        printf("Insert Option: ");
+        scanf("%d", &want);
+        getchar();  // Consume the '\n' character left by scanf
+        switch (want)
+        {
+        case 1:
+            /* code */
+            break;
+        case 2:
+            printf("Insert word: ");
+            fgets(word, 20, stdin);
+            word[strcspn(word, "\n")] = 0;  // Remove the newline character
+            if (strcmp(chosenWord, word) != 0)
+            {
+                printf("Wrong Word!\n");
+                lives--;
+            }
+            else{
+                printf("Congratulations! You Win!");
+                return;   
+            }
+            break;
+        default:
+            printf("Invalid Option.");
+            break;
+        }
+    }
+    printf("Game over! You have lost all your lives.\n");
+}
+
+int main(){
+
+    char playAgain = 'y';
+    while(playAgain == 'y' || playAgain == 'Y') {
+        game();
+        printf("Do you want to play again? (Y/N): ");
+        scanf(" %c", &playAgain);
+    }
+    printf("Thanks for playing :)\nBye...");
     return 0;
 }
+

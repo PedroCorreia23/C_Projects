ba4b9a74ffe30f2c065b7c60b9ea83f5bfceb8b5
Pedro <75038468+PedroCorreia23@users.noreply.github.com>
2023-06-25 14:10:42 +0100

    Update structs.h

--- studentManagementSystemProject/structs.h
+++ studentManagementSystemProject/structs.h
@@ -1,7 +1,41 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-struct Aluno
+struct Student
 {
-    char nome[50];
-    int idade, num; 
+    char name[50];
+    int ID, numberClasses;
+
 };
+
+struct Classes
+{
+    int classID;
+    char className[20];
+};
+
+//Each node will represent a student and will contain a Student struct
+struct Node {
+    struct Student student;
+    struct Node* next;
+};
+
+bool existFile(const char *filename){
+    
+    FILE *fp = fopen(filename, "r");
+    bool exist = false;
+    if (fp != NULL)
+    {
+        exist = true;
+        fclose(fp); // close the file
+    }
+    return exist;
+}
+
+void creatFile(const char *filename){
+    // Create a file
+    FILE *fp = fopen(filename, "w"); 
+
+    // Close the file
+    fclose(fp);
+}

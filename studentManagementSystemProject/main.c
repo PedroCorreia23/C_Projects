85030cfa8c972013473c18f7e9626851db4c0bba
Pedro <75038468+PedroCorreia23@users.noreply.github.com>
2023-06-25 17:16:57 +0100

    Update main.c

--- studentManagementSystemProject/main.c
+++ studentManagementSystemProject/main.c
@@ -1,19 +1,44 @@
 #include "structs.h"
 
+
 int main(){
 
     int option;
     char *filename = "students.txt";
 
-    if (existFile(filename) == false) //if file doesnt exist
+    if (!existFile(filename)) //if file doesnt exist
     {
         creatFile(filename);
     }
-    
-    
-    printf("\n=== MENU ===\n");
-    printf("1. Add Student\n2. Display All Students\n3. Search Student\n");
-    printf("4. Update Stundent Information\n5. Delete a student\n6. Calculate Grades\n");
-    printf("7. Attendance Management\n8. Exit");
 
+    Node* head = NULL;
+
+    while(1) {
+        printf("\n=== MENU ===\n");
+        printf("1. Add Student\n2. Display All Students\n3. Search Student\n");
+        printf("4. Update Stundent Information\n5. Delete a student\n6. Calculate Grades\n");
+        printf("7. Attendance Management\n8. Exit\n");
+        printf("Choose an option: ");
+        scanf("%d", &option);
+
+        if(option == 1) {
+            Student new_student;
+            printf("Enter student name: ");
+            scanf("%s", new_student.name);
+            printf("Enter student ID: ");
+            scanf("%d", &new_student.ID);
+            printf("Enter number of classes: ");
+            scanf("%d", &new_student.numberClasses);
+
+            addStudent(&head, new_student);
+        } else if (option == 8) {
+            break;
+        } else {
+            printf("Option not implemented yet!\n");
+        }
+    }
+
+    writeStudentsToFile(head, "students.txt");
+
+    return 0;
 }

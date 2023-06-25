cc8830e3266a8e577eb81d9115bf6b5f82347666
Pedro <75038468+PedroCorreia23@users.noreply.github.com>
2023-06-25 13:29:44 +0100

    Add files via upload

---
+++ studentManagementSystemProject/main.c
@@ -0,0 +1,17 @@
+#include "structs.h"
+
+
+
+
+int main(){
+
+
+    int option;
+    printf("\n=== MENU ===\n");
+    printf("1. Add Student\n2. Display All Students\n3. Search Student\n");
+    printf("4. Update Stundent Information\n5. Delete a student\n6. Calculate Grades\n");
+    printf("7. Attendance Management\n8. Exit");
+
+
+
+}
---
+++ studentManagementSystemProject/structs.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+struct Aluno
+{
+    char nome[50];
+    int idade, num; 
+};

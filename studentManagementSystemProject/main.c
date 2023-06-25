b0bd45fe544221eec0f6a6968cf4eaa4073e95ad
Pedro <75038468+PedroCorreia23@users.noreply.github.com>
2023-06-25 14:11:04 +0100

    Update main.c

--- studentManagementSystemProject/main.c
+++ studentManagementSystemProject/main.c
@@ -1,17 +1,18 @@
 #include "structs.h"
 
-
-
-
 int main(){
 
-
     int option;
+    char *filename = "students.txt";
+
+    if (existFile(filename) == false) //if file doesnt exist
+    {
+        creatFile(filename);
+    }
+    
     printf("\n=== MENU ===\n");
     printf("1. Add Student\n2. Display All Students\n3. Search Student\n");
     printf("4. Update Stundent Information\n5. Delete a student\n6. Calculate Grades\n");
     printf("7. Attendance Management\n8. Exit");
-
-
 
 }

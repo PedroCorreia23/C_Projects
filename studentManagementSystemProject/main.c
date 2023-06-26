a40640692088dce6446befcea2032fa1f46c3828
Pedro <75038468+PedroCorreia23@users.noreply.github.com>
2023-06-26 01:23:44 +0100

    Update main.c

--- studentManagementSystemProject/main.c
+++ studentManagementSystemProject/main.c
@@ -3,7 +3,7 @@
 
 int main(){
 
-    int option;
+    int option, c;
     char *filename = "students.txt";
 
     if (!existFile(filename)) //if file doesnt exist
@@ -12,33 +12,48 @@ int main(){
     }
 
     Node* head = NULL;
+    Student new_student;
 
     while(1) {
         printf("\n=== MENU ===\n");
         printf("1. Add Student\n2. Display All Students\n3. Search Student\n");
         printf("4. Update Stundent Information\n5. Delete a student\n6. Calculate Grades\n");
-        printf("7. Attendance Management\n8. Exit\n");
+        printf("7. Attendance Management\n8. Save\n9. Exit\n");
         printf("Choose an option: ");
         scanf("%d", &option);
+        while ((c = getchar()) != '\n' && c != EOF);
 
-        if(option == 1) {
-            Student new_student;
+        switch (option)
+        {
+        case 1:
+            
             printf("Enter student name: ");
-            scanf("%s", new_student.name);
+            fgets(new_student.name, 50, stdin);
+            new_student.name[strcspn(new_student.name, "\n")] = 0;
             printf("Enter student ID: ");
             scanf("%d", &new_student.ID);
+            while ((c = getchar()) != '\n' && c != EOF);
             printf("Enter number of classes: ");
             scanf("%d", &new_student.numberClasses);
+            while ((c = getchar()) != '\n' && c != EOF);
 
             addStudent(&head, new_student);
-        } else if (option == 8) {
             break;
-        } else {
-            printf("Option not implemented yet!\n");
+        case 2:
+            printf("\n====Students====\n");
+            printStudents(filename);
+            break;
+        case 8:
+            writeStudentsToFile(head, "students.txt");
+            printf("Data saved successfully.\n");
+            break;
+        case 9:
+            exit(0);
+        default:
+            printf("Invalid Option!");
+            break;
         }
     }
-
-    writeStudentsToFile(head, "students.txt");
 
     return 0;
 }
